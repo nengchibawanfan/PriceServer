f554ee9f186597f74c01b82276045e525504e7af
ChaoZhang <nengchibawanfan@163.com>
2019-03-23 00:49:15 +0800

    2019-03-23 晚

--- priceserver/test/response.py
+++ priceserver/test/response.py
@@ -1,32 +1,5 @@
-import graphene
-
-from priceserver.cal_price import calculate_price
-from utils.logger import getLog
-
-logger = getLog()
-
-
-class Price(graphene.ObjectType):
-    currency = graphene.String()
-    price = graphene.Float()
-
-
-class Symbol(graphene.ObjectType):
-    symbol = graphene.String()
-    price = graphene.List(Price)
-
-
-class Exchange(graphene.ObjectType):
-    exchange_name = graphene.String()
-    coin_list = graphene.List(Symbol)
-
-
-class Info(graphene.ObjectType):
-    # info = graphene.List(Exchange)
-    info = graphene.List(Exchange)
-
-
 response = {
+    "支持的交易所列表": ["bytetrade", "huobipro", "okex"],
     "info": [
         {
             "exchange_name": "huobipro",
@@ -39,7 +12,7 @@ response = {
                                    "currency": "USD",
                                    "price": 1000
                                }]},
-                          {"coin_name": "MT/BTC",
+                          {"coin_name": "MT/ETH",
                            "price": [{
                                "currency": "CNY",
                                "price": 1000
@@ -49,6 +22,7 @@ response = {
                                    "price": 1000
                                }]}
                           ],
+
         },
         {
             "exchange_name": "bytetrade",
@@ -95,61 +69,7 @@ response = {
                                    "price": 1000
                                }]}
                           ],
+
         }]
+
 }
-
-
-# 定义查询接口，类似于 GET
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-
-    def resolve_hello(self, info, **kwargs):
-        return "THIS IS PRICE_SERVER"
-
-    # 查询接口以及必须的参数
-    # graphene.String(required=True)
-    info = graphene.Field(Info)
-    exchange = graphene.List(Exchange)
-    symbol = graphene.List(Symbol)
-    price = graphene.List(Price)
-
-    def resolve_info(self, info, *args, **kwargs):
-        # obj = Info(
-        #     info=[self.resolve_exchange(info, exchange_name=exchange_name) for exchange_name in
-        #           ["huobipro", "bytetrade"]]
-        # )
-        obj = Info()
-        #
-        return obj
-
-    def resolve_exchange(self, info, exchange_name=None):
-        # if exchange_name:
-        #     # 解析exhangename
-        #     exchange_name = [exchange_name]
-        # else:
-        #     exchange_name = ["huobipro", "bytetrade", "okex"]
-
-        for exchange in exchange_name:
-            obj = Exchange(exchange_name=exchange_name,
-                           coin_list=[self.resolve_symbol(info, symbol_name=symbol_name, exchange_name=exchange_name) for
-                                      symbol_name in ["MT/ETH", "KCASH/ETH"]]
-                           )
-            return obj
-
-    def resolve_symbol(self, info, symbol_name, exchange_name):
-        obj = Symbol(symbol_name=symbol_name,
-                     price=[self.resolve_price(info, currency=currency, symbol_name=symbol_name,
-                                               exchange_name=exchange_name) for currency in ["CNY", "USD"]])
-
-        return obj
-
-    def resolve_price(self, info, currency, symbol_name, exchange_name):
-        start, mid = symbol_name.split("/")
-        end = currency
-        price = calculate_price(exchange_name, start, mid, end)
-
-        return Price(currency=currency, price=price)
-
-
-if __name__ == '__main__':
-    pass
---
+++ priceserver/test/test_log.py
@@ -0,0 +1,3 @@
+from utils.logger import getLog
+
+logger = getLog()
