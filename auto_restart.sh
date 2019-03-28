d3c5ed89fd6168b0e41fdaf5ea9c0f038c0f0a37
ChaoZhang <nengchibawanfan@163.com>
2019-03-28 12:03:02 +0800

    自动检查，挂掉重启，重启报警脚本

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 while true;
 do
-processExist=`ps aux | grep gunicorn | grep -v "grep" `
-if [ -z $processExist ];then
-echo "proecss is restarted"
-gunicorn -c gunicorn_conf.py new_price_server:app
-else
- echo "process is running"
-fi
-sleep 60 #每 60s检查一次
+    priceserverProcess=`ps aux | grep new_price_server | grep -v "grep" `
+    if [ -z $priceserverProcess ]
+    then
+        echo "priceserverProcess is restarted"
+        nohup gunicorn -c gunicorn_conf.py new_price_server:app &
+    else
+        echo "priceserverProcess is running"
+    fi
+    getPriceProcess=`ps aux | grep get_cu_price_and_sub_redis | grep -v "grep" `
+    if [ -z $getPriceProcess ]
+    then
+        echo "getPriceProcess is restarted"
+        nohup python get_cu_price_and_sub_redis.py &
+    else
+         echo "getPriceProcess is running"
+    fi
+    sleep 60 #每 60s检查一次
 done
