e8887114b39f53e3cca87360e716a174a4f2eaec
ChaoZhang <nengchibawanfan@163.com>
2019-04-12 14:06:43 +0800

    单例redis  不加锁

--- priceserver/common/db_connection.py
+++ priceserver/common/db_connection.py
@@ -2,15 +2,15 @@ import redis
 import threading
 
 class ConnectRedis(object):
-    _instance_lock = threading.Lock()
+    # _instance_lock = threading.Lock()
 
     def __init__(self):
         pass
 
     def __new__(cls, *args, **kwargs):
         if not hasattr(ConnectRedis, "r"):
-            with ConnectRedis._instance_lock:
-                if not hasattr(ConnectRedis, "r"):
+            # with ConnectRedis._instance_lock:
+            #     if not hasattr(ConnectRedis, "r"):
                     ConnectRedis.r = redis.StrictRedis(host="127.0.0.1", port=6379, decode_responses=True)
         return ConnectRedis.r
 
