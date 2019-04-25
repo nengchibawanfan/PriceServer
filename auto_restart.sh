d3e99b814fa8914ddfba8edc1d9344e020b9bbd1
ChaoZhang <nengchibawanfan@163.com>
2019-04-25 11:46:50 +0800

    修改脚本

--- auto_restart.sh
+++ auto_restart.sh
@@ -9,13 +9,5 @@ do
     else
         echo "priceserverProcess is running"
     fi
-    getPriceProcess=`ps aux | grep get_cu_price_and_sub_redis | grep -v "grep" `
-    if [ -z $getPriceProcess ]
-    then
-        echo "getPriceProcess is restarted"
-        nohup python get_cu_price_and_sub_redis.py &
-    else
-         echo "getPriceProcess is running"
-    fi
     sleep 60 #每 60s检查一次
 done
