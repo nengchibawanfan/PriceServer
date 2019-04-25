37ccdc63357a0f79f4b76c626314152c45f1db7a
ChaoZhang <nengchibawanfan@163.com>
2019-04-25 20:02:18 +0800

    监控数据获取重启并报警

--- Receive_the_data.py
+++ Receive_the_data.py
@@ -10,29 +10,16 @@ import time
 r = redis.StrictRedis(decode_responses=True)
 
 
-def push_bear():
-    PUSH_BEAR_KEY = "11970-ba5f3d1644a4bd880a04ebdef3560f69"
-    import requests
-    url = "https://pushbear.ftqq.com/sub"
-    data = {
-        "sendkey": PUSH_BEAR_KEY,
-        "text": "PriceServer——GraphQL",
-        "desp": "没收到数据重启"
-    }
-    headers = {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Accept-Encoding": "gzip, deflate, br",
-        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-    requests.post(url, data=data, headers=headers)
-
 
 while True:
     now = time.time()
-    bytetrade_data = r.get("Receive_the_data_bytetrade")
-    huobi_data = r.get("Receive_the_data_huobi")
+    bytetrade_data = float(r.get("Receive_the_data_bytetrade"))
+    huobi_data = float(r.get("Receive_the_data_huobi"))
+    coinbase_data = float(r.get("Receive_the_data_coinbase"))
 
     bytetrade = now - bytetrade_data
     huobi = now - huobi_data
+    coinbase = now - coinbase_data
 
     if bytetrade > 60 * 5:
         cmd_str = 'pm2 restart ' + "get_cu_price_and_sub_redis"
@@ -40,6 +27,11 @@ while True:
         result = os.system(cmd_str)
 
     if huobi > 60 * 5:
+        cmd_str = 'pm2 restart ' + "get_cu_price_and_sub_redis"
+
+        result = os.system(cmd_str)
+
+    if coinbase > 60 * 5:
         cmd_str = 'pm2 restart ' + "get_cu_price_and_sub_redis"
 
         result = os.system(cmd_str)
--- get_cu_price_and_sub_redis.py
+++ get_cu_price_and_sub_redis.py
@@ -84,6 +84,7 @@ class Quote(object):
                 k = symbol["base"] + "/" + symbol["currency"]
                 price = symbol["latest"]
                 # 将value写入到redis中
+                self.r.set("Receive_the_data_coinbase", time.time())
                 self.r.hset("coinbase_currency_price", k, price)
 
     def start(self):
@@ -200,6 +201,23 @@ class Quote(object):
 
 if __name__ == '__main__':
     # 开始的时候将原来的键删掉，构建新的  一旦加了新的交易对，重启程序
+
+    def push_bear():
+        PUSH_BEAR_KEY = "11970-ba5f3d1644a4bd880a04ebdef3560f69"
+        import requests
+        url = "https://pushbear.ftqq.com/sub"
+        data = {
+            "sendkey": PUSH_BEAR_KEY,
+            "text": "PriceServer——GraphQL",
+            "desp": "数据获取重启"
+        }
+        headers = {
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Encoding": "gzip, deflate, br",
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0"}
+        requests.post(url, data=data, headers=headers)
+
+    push_bear()
     r = ConnectRedis()
     r.delete("price_server_bytetrade")
     r.delete("price_server_huobipro")
