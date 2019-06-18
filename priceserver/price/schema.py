bd0bf9c9b153e19dd7ec4aab14e1fe042e906719
ChaoZhang <nengchibawanfan@163.com>
2019-06-18 14:37:50 +0800

    返回所有today

--- priceserver/price/schema.py
+++ priceserver/price/schema.py
@@ -20,8 +20,15 @@ class Symbol(graphene.ObjectType):
     today = graphene.String()
     price = graphene.List(Price)
 
+class Today(graphene.ObjectType):
+    symbol_name = graphene.String()
+    today = graphene.String()
+
+
 def create_symbol_obj(symbol_name, currency):
+
     if "/" in symbol_name:
+        # 看是MT/ETH这种,还是MT这种    获取这个的today 勇哥那边就不需要再查了
         today = r.hget("price_server_bytetrade_today", symbol_name)
     else:
         today = ""
@@ -31,9 +38,11 @@ def create_symbol_obj(symbol_name, currency):
     return obj
 
 def create_price_obj(symbol_name, currency):
+
     if "/" in symbol_name:
         symbol = symbol_name
     else:
+        # 优先计算兑ETH
         symbol = symbol_name + "/" + PRIORITY[0]
 
 
@@ -42,6 +51,7 @@ def create_price_obj(symbol_name, currency):
     # price = random.randrange(1000)
     obj = Price(currency=currency, price=price)
     return obj
+
 
 # 定义查询接口，类似于 GET
 class Query(graphene.ObjectType):
@@ -57,16 +67,17 @@ class Query(graphene.ObjectType):
 
     def resolve_symbols(self, info, symbol_name=None, currency=None, **kwargs):
         if currency:
+            # 传来的法币是一个字符串  用,分割
             temp = currency.split(",")
             currency_list = [i.replace(" ", "") for i in temp]
         else:
+            # 没传就返回所有的法币价格
             currency_list = CURRENCY_LIST
-        # 返回所有的
 
         if symbol_name:
+            # 如果传了市场的列表 分割
             temp = symbol_name.split(",")
             symbol_list = [i.replace(" ", "") for i in temp]
-
         else:
             symbol_list = SYMBOL_LIST
             # symbol_list = MARKET_LIST
@@ -79,6 +90,14 @@ class Query(graphene.ObjectType):
     def resolve_all_price(self, info, currency, **kwargs):
         pass
 
+    all_today = graphene.List(Today)
+    def resolve_all_today(self):
+        response = []
+        res = r.hgetall("price_server_bytetrade_today")
+        for k, v in res.items:
+            obj = Today(symbol_name=k, today=v)
+            response.append(obj)
+        return response
 
 if __name__ == '__main__':
     pass
