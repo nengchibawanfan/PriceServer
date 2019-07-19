81e8afe56107a558d1873cb576b1b19119f9cb56
zc <nengchibawanfan@163.com>
2019-07-19 20:38:33 +0800

    up

--- auto_restart.sh
+++ auto_restart.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 while true;
 do
-    priceserverProcess=`ps aux | grep new_price_server | grep -v "grep" `
+    priceserverProcess=`ps aux | grep new_new_price_server | grep -v "grep" `
     if [ -z $priceserverProcess ]
     then
         echo "priceserverProcess is restarted"
-        nohup gunicorn -c gunicorn_conf.py new_price_server:app &
+        pm2 start /home/ubuntu/newPriceServer/PriceServer/new_new_price_server
     else
         echo "priceserverProcess is running"
     fi
